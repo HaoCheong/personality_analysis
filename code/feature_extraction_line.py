bc6d71442f134eeca40c04aacbee8279023c4ad0
Hao Cheong <hao.cheong@outlook.com>
2022-07-04 12:07:16 +1000

    Created parsable syllable-word list, added fkgl column, count syllables, and others

--- code/feature_extraction_line.py
+++ code/feature_extraction_line.py
@@ -5,6 +5,22 @@
 
 import re
 from data_cleanup_line import *
+import pandas as pd
+
+# --------Helpers--------
+def get_word_sylb_dict():
+    print("USED")
+    word_sylb_dict = {}
+    sylb_df = pd.read_csv('syllable_count_ndup.csv')
+    for index, row in sylb_df.iterrows():
+        # print(row[2])
+        word_sylb_dict[row[1]] = row[2]
+
+    return word_sylb_dict
+
+## GLOBAL
+word_sylb_dict = get_word_sylb_dict()
+
 
 # Number of characters (including stop words + punctuation)
 def num_of_char(line):
@@ -34,6 +50,14 @@ def num_any_word(line):
     newline = remove_all_large_space(newline)
     return len(newline.split(" "))
 
+# Number of any words (not including stop words)
+def num_nstop_word(line):
+    newline = remove_num(line)
+    newline = remove_punctuation(newline)
+    newline = remove_stop_words(newline)
+    newline = remove_all_large_space(newline)
+    return len(newline.split(" "))
+
 # Number of different word + stop words
 def num_diff_word_stop(line):
     newline = remove_num(line)
@@ -52,8 +76,84 @@ def num_diff_word_nstop(line):
 # Number of sentences (Tokenised based on ". ")
 # Need to include ?, ..., !, and other enders
 def num_sentences(line):
-    return len(re.split(r'[(. )(! )(? )]', line))
+    return len(re.split(r'[.!?]{1}', line))
 
 # Average sentences length (Total word count / sentence count)
 def avg_sentence_length(line):
     return float(num_any_word(line))/(num_sentences(line))
+
+def avg_word_length(line):
+    return float(num_of_char(line)/num_any_word(line))
+
+# Most frequent word length
+def most_freq_word_length(line):
+    newline = remove_num(line)
+    newline = remove_punctuation(newline)
+    newline = remove_all_large_space(newline)
+    
+    word_array = newline.split(" ")
+    freq_dict = {}
+    for word in word_array:
+        if freq_dict.get(len(word)) == None:
+            freq_dict[len(word)] = 1
+        else:
+            freq_dict[len(word)] = freq_dict[len(word)] + 1
+    
+    max_val = 0
+    max_key = ""
+
+    for key in freq_dict.keys():
+        if freq_dict[key] > max_val:
+            max_val = freq_dict[key]
+            max_key = key
+    
+    return max_key
+
+# Most frequent sentence length
+def most_freq_sentence_length(line):
+    sentence_array = re.split(r'[.!?]{1}', line)
+    freq_dict = {}
+    for sen in sentence_array:
+        # print(sen)
+        if freq_dict.get(len(sen)) == None:
+            freq_dict[len(sen)] = 1
+        else:
+            freq_dict[len(sen)] = freq_dict[len(sen)] + 1
+    
+    max_val = 0
+    max_key = ""
+    for key in freq_dict.keys():
+        if freq_dict[key] > max_val and key != 1: #Mitigates the . . . sentences but might need future adjusting
+            max_val = freq_dict[key]
+            max_key = key
+    
+    return max_key
+
+# Number of stop words
+
+# Number of syllables, known words (Might need changing)
+def num_syllables(line):
+    newline = remove_num(line)
+    newline = remove_punctuation(newline)
+    newline = remove_stop_words(newline)
+    newline = remove_all_large_space(newline)
+    word_list = newline.split(" ")
+    total_sylb_count = 0
+    for word in word_list:
+        if (word_sylb_dict.get(word) != None):
+            total_sylb_count = total_sylb_count + word_sylb_dict[word]
+
+    return total_sylb_count
+
+# --------READABILITY INDEX--------
+def flesch_kincaid_grade_level(line):
+    num_nstop_word_var = num_nstop_word(line)
+    num_sentences_var = num_sentences(line)
+    num_syllables_var = num_syllables(line)
+
+    comp_1 = 0.39 * float(num_nstop_word_var/num_sentences_var)
+    comp_2 = 11.8 * float(num_syllables_var/num_nstop_word_var)
+    fkgl = float(comp_1 + comp_2 - 15.59)
+    return fkgl
+
+
--- code/preprocess_pipeline.py
+++ code/preprocess_pipeline.py
@@ -6,6 +6,9 @@ import pandas as pd
 import spacy
 import string
 from nltk.corpus import stopwords
+import sys
+from os.path import exists
+
 
 import re
 stop = stopwords.words('english')
@@ -37,30 +40,45 @@ def required_preprocess(essays):
 
 # Pipeline for feature extraction
 def feature_extract(essays):
-    essays = process_column(essays, 'TEXT', 'num_of_char', num_of_char) # Always missing 2568
-    essays = process_column(essays, 'TEXT', 'num_any_words', num_any_word) # Correct 658
-    essays = process_column(essays, 'TEXT','num_long_words', num_long_words) # Correct 112
-    # essays = process_column(essays, 'TEXT','num_short_words', num_short_words) 
 
-    # essays = process_column(essays, 'TEXT','num_sentence', num_sentences)
-    # essays = process_column(essays, 'TEXT', "num_diff_word_stop", num_diff_word_stop)
-    # essays = process_column(essays, 'TEXT', "num_diff_word_nstop", num_diff_word_nstop)
-    # essays = process_column(essays, 'TEXT', 'avg_sentence_length',avg_sentence_length)
+    
+    essays = process_column(essays, 'TEXT', 'num_of_char', num_of_char) # Always missing 2 2568
+    essays = process_column(essays, 'TEXT', 'num_any_words', num_any_word) # Correct 658, always 1 off
+    essays = process_column(essays, 'TEXT','num_long_words', num_long_words) # Correct 109, 3 off, 112 after diff check
+    essays = process_column(essays, 'TEXT','num_short_words', num_short_words) # Counterd 548, 2 off
+
+    essays = process_column(essays, 'TEXT','num_sentences', num_sentences) # Correct 38
+    essays = process_column(essays, 'TEXT', "num_diff_word_stop", num_diff_word_stop) # Correct 272
+    essays = process_column(essays, 'TEXT', "num_diff_word_nstop", num_diff_word_nstop)
+    essays = process_column(essays, 'TEXT', 'avg_sentence_length',avg_sentence_length)
+    essays = process_column(essays, 'TEXT', 'avg_word_length',avg_word_length)
+
+    essays = process_column(essays, 'TEXT', 'num_syllables',num_syllables)
+
+    essays = process_column(essays, 'TEXT', 'most_freq_word_length',most_freq_word_length) 
+    essays = process_column(essays, 'TEXT', 'most_freq_sentence_length',most_freq_sentence_length)
+
+    # Readability
+    essays = process_column(essays, 'TEXT', 'flesch_kincaid_grade_level',flesch_kincaid_grade_level)
+
 
     return essays
 
 def main():
-    essays = pd.read_csv('essays.csv', encoding='cp1252')
-    processed_essays = required_preprocess(essays)
-
-    processed_essays.to_csv('pre_processed.csv', sep=',', encoding='utf-8', index = False)
-
-    test_line = processed_essays['TEXT'][0]
+    processed_essays = None
+    if (not exists('pre_processed.csv')):
+        essays = pd.read_csv('essays.csv', encoding='cp1252')
+        processed_essays = required_preprocess(essays)
+        processed_essays.to_csv('pre_processed.csv', sep=',', encoding='utf-8', index = False)
+    else:
+        processed_essays = pd.read_csv('pre_processed.csv', encoding='utf-8')
     
+    test_line = processed_essays['TEXT'][5]
+
+    print(flesch_kincaid_grade_level(test_line))
     final_processed = feature_extract(processed_essays)
 
-    # print((num_long_words(test_line)))
-    print(final_processed.head(1))
+    print(final_processed.head(10))
 
     # final_processed.to_csv('final.csv', sep=',', encoding='utf-8', index = False)
 
