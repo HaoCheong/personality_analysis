a52c7e699d14cc9af90b91d1ca74634975b5a227
Hao Cheong <hao.cheong@outlook.com>
2022-07-26 18:17:21 +1000

    Added a number of feature analysis code, shifted things to isolatted code blocks

--- code/feature_analysis.py
+++ code/feature_analysis.py
@@ -1,9 +1,13 @@
 from re import I
+
 import numpy as np
 import sys
 
 import pandas as pd
 import warnings
+
+from scipy.stats import skew
+
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 if len(sys.argv) != 2:
@@ -12,22 +16,19 @@ if len(sys.argv) != 2:
 
 csv_file = sys.argv[1]
 feature_df = pd.read_csv(csv_file, index_col = 0)
-analysis_pipeline = []
-column_names = []
-index = []
 
-# Carry out calculation off all in the analysis pipeline
-def comparison_calc(df_col):
 
-    df = pd.DataFrame(columns = ['feature_pair'] + column_names)
+def pearson_correlation(df_cols):
+    df = pd.DataFrame(columns = ['feature_pair'])
 
     i = 0
-    while (i < len(df_col)):
+    while (i < len(df_cols)):
         j = i + 1
-        while (j < len(df_col)):
-            res = {'feature_pair':f"{df_col[i]}_{df_col[j]}"}
-            for analysis in analysis_pipeline:
-                res[analysis[0]] = analysis[1](df_col[i],df_col[j])
+        while (j < len(df_cols)):
+            res = {'feature_pair':f"{df_cols[i]}_{df_cols[j]}"}
+            x = feature_df[df_cols[i]].to_numpy()
+            y = feature_df[df_cols[j]].to_numpy()
+            res['pearson_correlation'] = np.corrcoef(x,y)[0][1]
                 
             df = df.append(res, ignore_index=True)
             j = j + 1
@@ -36,28 +37,48 @@ def comparison_calc(df_col):
     
     return df
 
-# Add analysis to the pipeline
-def add_analysis(name, func):
-    analysis_tuple = (name,func)
-    analysis_pipeline.append(analysis_tuple)
-    column_names.append(name)
+# Calculating Skewness
+def skewness(df_cols):
+    df = pd.DataFrame(df_cols)
+    df.columns=['features']
+    # print(df.head(10))
+    df['skewness'] = df['features'].apply(lambda x: skew(np.asarray(feature_df[x].tolist())))
+    
+    return df
 
-# Get all the feature to process
-def feature_analysis():
-    add_analysis("pearson_corrcoef", pearson_correlation)
+def variance(df_cols):
+    df = pd.DataFrame(df_cols)
+    df.columns=['features']
+    df['variance'] = df['features'].apply(lambda x: np.var(np.asarray(feature_df[x].tolist())))
+    return df
 
-# Calculating Pearson Correlation
-def pearson_correlation(col_name_1, col_name_2):
-    x = feature_df[col_name_1].to_numpy()
-    y = feature_df[col_name_2].to_numpy()
-    return np.corrcoef(x,y)[0][1]
+def covariance(df_cols):
+    df = pd.DataFrame(columns = ['feature_pair'])
+
+    i = 0
+    while (i < len(df_cols)):
+        j = i + 1
+        while (j < len(df_cols)):
+            res = {'feature_pair':f"{df_cols[i]}_{df_cols[j]}"}
+            x = feature_df[df_cols[i]].to_numpy()
+            y = feature_df[df_cols[j]].to_numpy()
+            res['covariance'] = np.cov(x,y)[0][1]
+                
+            df = df.append(res, ignore_index=True)
+            j = j + 1
+
+        i = i + 1
+    
+    return df
     
 def main():
     reduced = feature_df.columns[6:36]
-    feature_analysis()
-    final_df = comparison_calc(reduced)
-    final_df.to_csv('final_analysis.csv', sep=',', encoding='utf-8', index = False)
-    print(comparison_calc(reduced).head(10))
+    ps_corr = pearson_correlation(reduced)
+    skw = skewness(reduced)
+    vari = variance(reduced)
+    covar = covariance(reduced)
+    covar.to_csv('covar.csv', sep=',', encoding='utf-8', index = False)
+
 
 if __name__ == "__main__":
     main()
