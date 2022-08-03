59f54b186d13ebab8cbd4e755b713c7dd9cceda5
Hao Cheong <hao.cheong@outlook.com>
2022-08-03 10:27:48 +1000

    Added a z_score finder, and preplotted the mass plot

--- code/feature_analysis.py
+++ code/feature_analysis.py
@@ -1,35 +1,39 @@
-from re import I
-
-import numpy as np
 import sys
 
+import numpy as np
+from scipy.stats import skew, kurtosis
+import scipy.stats as stats
+
 import pandas as pd
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+
 import warnings
-
-from scipy.stats import skew
-
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
+# Usage check, requires CSV file to process
 if len(sys.argv) != 2:
     print('USAGE: python3 feature_plotter.py <csv_file>')
     exit()
 
+# Global
 csv_file = sys.argv[1]
 feature_df = pd.read_csv(csv_file, index_col = 0)
+analysis_df = {}
 
-
+# Calculate pearson correlation coefficient
 def pearson_correlation(df_cols):
-    df = pd.DataFrame(columns = ['feature_pair'])
+    df = pd.DataFrame(columns = ['feature_1', 'feature_2'])
 
     i = 0
     while (i < len(df_cols)):
+        # Skips comparing i with j column
         j = i + 1
         while (j < len(df_cols)):
-            res = {'feature_pair':f"{df_cols[i]}_{df_cols[j]}"}
+            res = {'feature_1':f"{df_cols[i]}", 'feature_2':f"{df_cols[j]}"}
             x = feature_df[df_cols[i]].to_numpy()
             y = feature_df[df_cols[j]].to_numpy()
-            res['pearson_correlation'] = np.corrcoef(x,y)[0][1]
-                
+            res['ps_corr'] = np.corrcoef(x,y)[0][1]
             df = df.append(res, ignore_index=True)
             j = j + 1
 
@@ -37,29 +41,36 @@ def pearson_correlation(df_cols):
     
     return df
 
-# Calculating Skewness
+# Calculate Skewness
 def skewness(df_cols):
     df = pd.DataFrame(df_cols)
     df.columns=['features']
-    # print(df.head(10))
     df['skewness'] = df['features'].apply(lambda x: skew(np.asarray(feature_df[x].tolist())))
-    
     return df
 
+# Calculate Variance
 def variance(df_cols):
     df = pd.DataFrame(df_cols)
     df.columns=['features']
     df['variance'] = df['features'].apply(lambda x: np.var(np.asarray(feature_df[x].tolist())))
     return df
 
+# Calculate Kurtosis
+def ktosis(df_cols):
+    df = pd.DataFrame(df_cols)
+    df.columns=['features']
+    df['kurtosis'] = df['features'].apply(lambda x: kurtosis(feature_df[x].tolist()))
+    return df
+
+# Calculate Covariance
 def covariance(df_cols):
-    df = pd.DataFrame(columns = ['feature_pair'])
+    df = feature_df(['feature_1', 'feature_2'])
 
     i = 0
     while (i < len(df_cols)):
         j = i + 1
         while (j < len(df_cols)):
-            res = {'feature_pair':f"{df_cols[i]}_{df_cols[j]}"}
+            res = {'feature_1':f"{df_cols[i]}", 'feature_2':f"{df_cols[j]}"}
             x = feature_df[df_cols[i]].to_numpy()
             y = feature_df[df_cols[j]].to_numpy()
             res['covariance'] = np.cov(x,y)[0][1]
@@ -68,17 +79,53 @@ def covariance(df_cols):
             j = j + 1
 
         i = i + 1
-    
     return df
-    
-def main():
-    reduced = feature_df.columns[6:36]
-    ps_corr = pearson_correlation(reduced)
-    skw = skewness(reduced)
-    vari = variance(reduced)
-    covar = covariance(reduced)
-    covar.to_csv('covar.csv', sep=',', encoding='utf-8', index = False)
 
+# Return z-score of the values instead
+def z_score(df_cols):
+    df = feature_df[['TEXT','cEXT','cNEU','cAGR','cCON','cOPN']]
+    for col in df_cols:
+        df[f'{col}_z_score'] = stats.zscore(np.asarray(feature_df[col]))
+
+    return df
+
+# Analyse individual feature
+def analyse(cols, name, func, reproc):
+    if (reproc):
+        result = func(cols)
+        result.to_csv('./analysis_data/'+name+'.csv', sep=',', encoding='utf-8', index = False)
+        analysis_df[name] = result
+    else:
+        result = pd.read_csv('./analysis_data/'+name+'.csv')
+        analysis_df[name] = result
+
+# Analyse all the feature, given columns and reprocess boolean
+def feature_analysis(cols, reproc = False):
+    analyse(cols, 'ps_corr', pearson_correlation, reproc)
+    analyse(cols, 'skw', skewness, reproc)
+    analyse(cols, 'vari', variance, reproc)
+    analyse(cols, 'covari', covariance, reproc)
+    analyse(cols, 'kurtosis', ktosis, reproc)
+
+def main():
+
+    # All extracted features
+    reduced = feature_df.columns[6:36]
+    z_score(reduced).to_csv(f'./z_score_{csv_file}', sep=',', encoding='utf-8')
+    # feature_analysis(reduced, True)
+
+    # ps_corr = analysis_df['ps_corr']
+    # ps_corr_signf = ps_corr[abs(ps_corr['ps_corr']) > 0.5]
+    # print(ps_corr_signf)
+
+    # skw = analysis_df['skw']
+    # skw_normal = skw[skw['skewness'] < 1]
+    # skw_normal = skw_normal[skw_normal['skewness'] > -1]
+    # print(skw_normal)
+
+    # print(kurtosis(np.asarray(feature_df['num_of_char'])))
+
+    print(" ====== DONE ====== ")
 
 if __name__ == "__main__":
     main()
--- code/feature_plotter.py
+++ code/feature_plotter.py
@@ -55,8 +55,12 @@ csv_file = sys.argv[1]
 feature_df = pd.read_csv(csv_file, index_col = 0)
 
 # Simple plotting field/column
-def basic_plot(field):
+def dis_plot(field):
     sns.displot(feature_df[field])
+
+# Simple plotting field/column
+def hist_plot(field):
+    sns.histplot(feature_df[field])
 
 # Plot 2 fields against each other
 def two_col_pairplot(field1, field2):
@@ -65,7 +69,8 @@ def two_col_pairplot(field1, field2):
 # Plot every field again every field
 def multi_pairplot(reduced_col):
     sns.pairplot(feature_df[reduced_col], markers=["o", "s"])
-    
+
+# Plot every column in col_names again each other
 def mass_plotter(col_names):
     i = 0
     while (i < len(col_names)):
@@ -78,12 +83,30 @@ def mass_plotter(col_names):
 
         i = i + 1
 
+# Bar plot for single field
+def bar_plot(field):
+    sns.barplot(feature_df[field])
+
+# Generate 
+def solo_feature_plot(col_names, name, func):
+    for field in col_names:
+        func(field)
+        plt.savefig('./plots/' + name + '/' + field)
+
 def main():
-    # all_features = feature_df.columns[6:36].tolist()
-    reduced = feature_df[['num_long_words','num_short_words','num_diff_word_stop','num_diff_word_nstop','num_of_char','num_any_words']].copy()
-    # multi_pairplot(reduced.columns)
-    mass_plotter(reduced.columns)
+    all_features = feature_df.columns[6:36].tolist()
+    # reduced = feature_df[['flesch_kincaid_grade_level','avg_l']].copy()
+    # mass_plotter(reduced.columns)
+    multi_pairplot(all_features)
     # plt.savefig('./plots/' + plot_filename)
+    plt.savefig('./plots/z_score_plot')
+
+
+    # solo_feature_plot(all_features, "displots", dis_plot)
+    # solo_feature_plot(all_features, "histplots", hist_plot)
+
+    # dis_plot('num_of_char')
+    # plt.savefig('./plots/temp')
 
 if __name__ == "__main__":
     main()
